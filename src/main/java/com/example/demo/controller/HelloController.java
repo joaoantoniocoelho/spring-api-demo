090240f7254bd56b701904d57843cb2e33569b19
João Coelho <joaoantonioscoelho@gmail.com>
2025-08-05 23:16:29 -0300

    fix hello controller

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @GetMapping("hello")
+    @GetMapping("/hello")
     public ResponseEntity<String> hello() {
         return ResponseEntity.ok("Hello, world!");
     }
